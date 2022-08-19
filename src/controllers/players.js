6a7bcace0a9409d92f6ec216e3fff75b85fe2b27
Alfredo Rodriguez <fredd.073100@gmail.com>
2022-08-19 16:15:59 -0500

    Adding some comments to code and modifying if when we search a user by email

--- src/controllers/players.js
+++ src/controllers/players.js
@@ -4,6 +4,7 @@ const { userLogged } = require('../helpers/playerLoggedHandler');
 
 const playerController = {
   getAllPlayers: async (req, res) => {
+    // Get all users from database
     const allPlayers = await playerService.getAll();
 
     res.status(200).json({
@@ -15,6 +16,8 @@ const playerController = {
 
   getPlayerById: async (req, res) => {
     const { id } = req.params;
+
+    // Get player by id
     const player = await playerService.getOne(id);
 
     res.status(200).json({ status: 200, data: player });
@@ -23,6 +26,7 @@ const playerController = {
   createPlayer: async (req, res) => {
     const newPlayer = { ...req.body };
 
+    //Create a new user
     const playerCreated = await playerService.create(newPlayer);
 
     res.status(201).json({ status: 201, data: playerCreated });
@@ -30,12 +34,17 @@ const playerController = {
 
   loginPlayer: async (req, res) => {
     const { email, password } = req.body;
+
+    // Found player in db by email
     const playerFound = await playerService.login(email);
 
-    if (playerFound === null || undefined) {
+    if (!playerFound) {
       res.status(403).json({ status: 403, data: 'Usuario no encontrado' });
     } else {
+      // Comparing password given from frontEnd and validate it the hash password
       const player = await compare(password, playerFound.password);
+
+      // Getting a object data if player
       const { status, data } = userLogged(player);
 
       res.status(status).json({ status: status, data: data });
